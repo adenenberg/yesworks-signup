158fe1b0b3d0469e6ca3177aa37b80672d8df03e
Addison Denenberg <adenenberg@credera.com>
2014-01-29 14:58:12 -0600

    Simple Hello World page working

---
+++ src/main/java/com/yesworks/signup/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.yesworks.signup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomeController {
+
+	@RequestMapping("/")
+	public String welcomePage(Model model) {
+		
+		return "/jsp/home";
+	}
+}
